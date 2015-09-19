5b608d306cb6bfc3a6ba1f007acee21634eea1ff
Sherard Bailey <sbailey@xogrp.com>
2015-09-19 18:44:59 -0400

    Add socket.io

--- public/javascripts/lib/base.js
+++ public/javascripts/lib/base.js
@@ -1,11 +1,17 @@
 jQuery(function($){
   // Load the models
+  
   var termCollection = new TermCollection();
   var termListView = new TermListView({ el: '#terms', collection: termCollection });
   termCollection
     .fetch({ reset: true })
     .done(function(){
       // Setup Socket.io
+
+      var socket = io();
+      socket.on('terms', function (term) {
+        termCollection.add(term)
+      });
     })
     .fail(function(){
       $.error('Could not fetch the collection');
--- routes/terms.js
+++ routes/terms.js
@@ -2,35 +2,39 @@ var Term = require('../models/term');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/terms', function(req, res, next) {
-  Term
-    .find({})
-    .exec(function(err, terms){
-      res.format({
-        html: function(){
-          res.render('terms', {
-            terms: terms
-          })
-        },
+module.exports = function(io){
 
-        json: function(){
-          res.json(terms)
-        }
+  /* GET users listing. */
+  router.get('/terms', function(req, res, next) {
+    Term
+      .find({})
+      .exec(function(err, terms){
+        res.format({
+          html: function(){
+            res.render('terms', {
+              terms: terms
+            })
+          },
+
+          json: function(){
+            res.json(terms)
+          }
+        });
       });
+  });
+
+  router.post('/terms', function(req, res, next){
+    var term = new Term({
+      description: req.body.description
     });
-});
 
-router.post('/terms', function(req, res, next){
-  var term = new Term({
-    description: req.body.description
+    term.save(function(err, term){
+      if (err) throw err;
+
+      io.sockets.emit('terms', term);
+      res.send(term);
+    });
   });
 
-  term.save(function(err, term){
-    if (err) throw err;
-
-    res.send(term);
-  });
-});
-
-module.exports = router;
+  return router;
+};
